d27a84f2eed98083b3f5dd5d6a062e1e23010785
King-Shilencio <34038871+King-Shilencio@users.noreply.github.com>
2017-11-30 13:08:29 -0600

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Final
+{
+    class Game
+    {
+        public static Encounter[] encounters;
+        public static Room[] roomNumber;
+        
+        public static int enCounter()
+        {
+            int encounter = 0;
+            int cnt = 0;
+            //counts the encounter
+            for (int x = 0; x < encounter; x++)
+            {
+                cnt += encounter;
+            }
+            return cnt;
+        }
+    }
+}
